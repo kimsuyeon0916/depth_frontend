83e7bffbab5149de195857a838d05e81b5aa44cd
Chang-KY <pzxcvp0508@gmail.com>
2025-12-10 20:37:36 +0900

    feat: ui button cancel 추가

---
+++ src/components/ui/button/Button.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import React from 'react'
+import './Button.css'
+import clsx from 'clsx'
+
+type ButtonVariant = 'add' | 'cancel'
+
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  children: React.ReactNode
+  icon?: React.ReactNode
+  variant?: ButtonVariant
+}
+
+const Button = (props: ButtonProps) => {
+  const { children, className, variant = 'add', icon, ...rest } = props
+  const variantClass = {
+    add: 'btn-add',
+    cancel: 'btn-cancel',
+  }[variant]
+
+  return (
+    <button {...rest} className={clsx('btn-basic', variantClass, className)}>
+      {icon && <div>{icon}</div>}
+      <span>{children}</span>
+    </button>
+  )
+}
+
+export default Button
