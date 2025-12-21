df94abeee33ced38e91e2484b1ad7d7950096ef7
kimsuyeon_0916 <kimsuyeon_0916@naver.com>
2025-12-21 23:13:08 +0900

    refactor: 트렌드 섹션 제거
    - 백엔드 미지원으로 제거

--- src/app/(authenticated)/(board)/layout.tsx
+++ src/app/(authenticated)/(board)/layout.tsx
@@ -9,7 +9,7 @@ export const TABS = [
   { id: TOPIC_TYPE.ALL, label: TOPIC_LABEL.ALL, href: '/' },
   { id: TOPIC_TYPE.NOTICE, label: TOPIC_LABEL.NOTICE, href: '/notice' },
   { id: TOPIC_TYPE.EMPLOYMENT_TIP, label: TOPIC_LABEL.EMPLOYMENT_TIP, href: '/job-tips' },
-  { id: TOPIC_TYPE.TREND, label: TOPIC_LABEL.TREND, href: '/trend' },
+  // { id: TOPIC_TYPE.TREND, label: TOPIC_LABEL.TREND, href: '/trend' },
   { id: TOPIC_TYPE.KNOWLEDGE, label: TOPIC_LABEL.KNOWLEDGE, href: '/knowledge' },
 ]
 
