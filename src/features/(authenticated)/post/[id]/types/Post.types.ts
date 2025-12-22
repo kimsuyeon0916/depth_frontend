70cc5f232c8bc1e1a2806abe1bd93c7ddb662756
kimsuyeon_0916 <kimsuyeon_0916@naver.com>
2025-12-22 10:24:25 +0900

    feat: Post 관련 타입 수정
    - Post에 highlightType, reactions 필드 추가
    - ReactionType, Reaction, PostCard 타입 추가
    - GetPostsResponse 타입 수정

--- src/features/(authenticated)/post/[id]/types/Post.types.ts
+++ src/features/(authenticated)/post/[id]/types/Post.types.ts
@@ -9,19 +9,30 @@ export type Post = {
   wroteAt: string
   content: string
   tags?: string[] // TODO: 필요 여부 검토
-  // TODO: 리액션 관련 프로퍼티 추가 예정
+  highlightType: HighlightType
   commentsCount: number
+  reactions: Reaction[]
+}
+
+export type ReactionType = 'LIKE' | 'HEART' | 'SMILE'
+
+export type Reaction = {
+  reactionType: ReactionType
+  count: number
+  reactedByMe?: boolean
 }
 
 export type HighlightType = 'BY_ADMIN' | 'BY_REACTION' | 'BY_VIEW' | 'NONE'
 
 export type GetPostDetailResponse = Post
 
+export type PostCard = Omit<Post, 'content'>
+
 export type GetPostsResponse = {
-  contents: Post[]
+  contents: PostCard[]
   // TODO: 무한스크롤 적용시 프로퍼티 추가 예정
-  hasNext: boolean
-  nextCursorId: number | null
+  hasNext?: boolean
+  nextCursorId?: number | null
 }
 
 export type CreatePostRequest = {
