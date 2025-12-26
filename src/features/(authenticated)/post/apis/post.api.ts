4b960175915a0481d9c6613ccab9183d45caacfb
kimsuyeon_0916 <kimsuyeon_0916@naver.com>
2025-12-26 22:29:07 +0900

    feat: 게시글 전체 조회 API 변경
    - 쿼리 스트링에 topic, page, size 반영

--- src/features/(authenticated)/post/apis/post.api.ts
+++ src/features/(authenticated)/post/apis/post.api.ts
@@ -10,18 +10,17 @@ import {
 } from '../types/Post.types'
 
 type GetPostsParams = {
-  cursorId?: number
+  page?: number
   size?: number
   topic?: TopicType
 }
 
-// TODO: getPosts가 topic/cursor/size 등을 쿼리로 받을 수 있게 되면, 전체 조회 후 프론트 필터링을 제거하고 서버에서 topic 필터링된 결과만 받아오도록 변경.
 export async function getPosts(params: GetPostsParams = {}): Promise<GetPostsResponse> {
   const searchParams = new URLSearchParams()
 
   if (params.topic) searchParams.set('topic', params.topic)
-  if (typeof params.cursorId === 'number') searchParams.set('cursorId', String(params.cursorId))
-  if (typeof params.size === 'number') searchParams.set('size', String(params.size))
+  if (typeof params.page === 'number') searchParams.set('page', String(params.page))
+  if (params.size) searchParams.set('size', String(params.size))
   const query = searchParams.toString()
 
   const res = await server(`/api/v1/posts/summary${query ? `?${query}` : ''}`, {
