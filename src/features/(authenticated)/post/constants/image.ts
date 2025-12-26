4b5f82677234da5ac294645ce45c20103418d9d6
kimsuyeon_0916 <kimsuyeon_0916@naver.com>
2025-12-27 01:32:26 +0900

    feat: 이미지 관련 상수 추가
    - ALLOWED_MIME_TYPES
    - MAX_IMAGE_BYTES
    - MAX_GIF_BYTES

---
+++ src/features/(authenticated)/post/constants/image.ts
@@ -0,0 +1,3 @@
+export const ALLOWED_MIME_TYPES = new Set(['image/jpeg', 'image/png', 'image/gif', 'image/webp'])
+export const MAX_IMAGE_BYTES = 5 * 1024 * 1024 // 5MB
+export const MAX_GIF_BYTES = 3 * 1024 * 1024 // 3MB
