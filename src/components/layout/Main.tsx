302ed6f350444be554afb943e96bee78eaad7c1a
kimsuyeon_0916 <kimsuyeon_0916@naver.com>
2025-12-27 15:20:23 +0900

    refactor: Main 레이아웃 컴포넌트 수정
    - 불필요한 px 제거

--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default async function Main({ children }: { children: React.ReactNode }) {
   return (
     <div className="bg-gray-50">
-      <div className="mx-auto flex w-full flex-col gap-5 px-0 lg:flex-row lg:px-5">{children}</div>
+      <div className="mx-auto flex w-full flex-col gap-5 lg:flex-row">{children}</div>
     </div>
   )
 }
